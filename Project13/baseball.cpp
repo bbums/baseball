7c9a2bfaada141d8dd028c8f890a6f07ad6fe438
Beomsu Kim <beomsu7.kim@samsung.com>
2025-07-02 13:17:28 +0900

    [feature] p27

--- Project13/baseball.cpp
+++ Project13/baseball.cpp
@@ -1,3 +1,9 @@
+#include <string>
+#include <stdexcept>
+
 class Baseball {
 public:
+	void guess(const std::string& string) {
+		throw std::length_error("Must be three letters.");
+	}
 };
--- Project13/baseball_test.cpp
+++ Project13/baseball_test.cpp
@@ -1,8 +1,11 @@
 #include "gmock/gmock.h"
 #include "baseball.cpp"
 
-TEST(BaseballGame, TryGameTest) {
-	EXPECT_EQ(1, 1);
+using namespace testing;
+
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
+	Baseball game;
+	EXPECT_THROW(game.guess(std::string("12")), std::length_error);
 }
 
 int main() {
