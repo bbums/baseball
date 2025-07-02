f8538b76ee479b123e3a518af8769ee34b10bd12
Beomsu Kim <beomsu7.kim@samsung.com>
2025-07-02 13:41:36 +0900

    [refactor] p38

--- Project13/baseball.cpp
+++ Project13/baseball.cpp
@@ -2,6 +2,14 @@
 #include <stdexcept>
 
 class Baseball {
+	bool itHasDup(const std::string& str) {
+		for (int c1 = 0; c1 < str.length(); c1++) {
+			for (int c2 = c1 + 1; c2 < str.length(); c2++) {
+				if (str[c1] == str[c2])
+					throw std::invalid_argument("똑바로 던지라고");
+			}
+		}
+	}
 public:
 	void guess(const std::string& str) {
 		if(str.length() != 3)
@@ -10,11 +18,7 @@ public:
 			if (ch1 < '0' || ch1 > '9')
 				throw std::invalid_argument("똑바로 던져");
 		}
-		for (int c1 = 0; c1 < str.length(); c1++) {		
-			for (int c2 = c1 + 1; c2 < str.length(); c2++) {
-				if(str[c1] == str[c2])
-					throw std::invalid_argument("똑바로 던지라고");
-			}
-		}
+		if(itHasDup(str))
+			throw std::invalid_argument("똑바로 던지라고");
 	}
 };
--- Project13/baseball_test.cpp
+++ Project13/baseball_test.cpp
@@ -3,19 +3,26 @@
 
 using namespace testing;
 
-TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
+class BaseballGameFixture : public Test {
+public:
 	Baseball game;
-	EXPECT_THROW(game.guess(std::string("12")), std::length_error);
+	void assertIllegalArgument(std::string guessNumber) {
+		try {
+			game.guess(guessNumber);
+			FAIL();
+		}
+		catch (std::exception e) {
+		}
+	}
+};
+
+TEST_F(BaseballGameFixture, ThrowExceptionWhenInputLengthIsUnmatched) {
+	assertIllegalArgument(std::string("12"));
 }
 
-TEST(BaseballGame, ThrowExceptionWhenInvalidChar) {
-	Baseball game;
-	EXPECT_THROW(game.guess(std::string("12s")), std::invalid_argument);
-}
-
-TEST(BaseballGame, ThrowExceptionWhenInvalidCase) {
-	Baseball game;
-	EXPECT_THROW(game.guess(std::string("121")), std::invalid_argument);
+TEST_F(BaseballGameFixture, ThrowExceptionWhenInvalidCase) {
+	assertIllegalArgument(std::string("12s"));
+	assertIllegalArgument(std::string("121"));
 }
 
 int main() {
