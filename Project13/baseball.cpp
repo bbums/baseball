b83f9ba14cb8920be57fe5a4ab3e3d74ad78b2f1
Beomsu Kim <beomsu7.kim@samsung.com>
2025-07-02 13:23:52 +0900

    [refactor] p29

--- Project13/baseball.cpp
+++ Project13/baseball.cpp
@@ -6,8 +6,8 @@ public:
 	void guess(const std::string& str) {
 		if(str.length() != 3)
 			throw std::length_error("Must be three letters.");
-		for (int c = 0; c < 3; c++)
-			if (str[c] < '0' || str[c] > '9') 
+		for (char ch : str)
+			if (ch < '0' || ch > '9')
 				throw std::invalid_argument("똑바로 던져");
 	}
 };
